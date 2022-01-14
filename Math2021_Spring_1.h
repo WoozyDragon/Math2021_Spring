5c4326d57a86560b7061d808ffc7b7a1805a14eb
Ex-32 <77823377+Ex-32@users.noreply.github.com>
2022-01-14 13:05:51 -0600

    added a "if not defined" style header guard
    added a "if not defined" style header guard because "#pragma once" is not officially defined and doesn't work with all compilers

--- Math2021_Spring_1.h
+++ Math2021_Spring_1.h
@@ -3,9 +3,14 @@
 
 #pragma once
 
+#ifndef MATH2021_SPRING_1_H
+#define MATH2021_SPRING_1_H
+
 #include <iostream>
 #include <cmath>
 #include <vector>
 
 //returns true if value is prime, false otherwise
 bool isPrime(int value);
+
+#endif
